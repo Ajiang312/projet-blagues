7e8bb8bef539bbca5193f3a08379f796f2a4cbe9
Antoine <antoine.jiang@esme.fr>
2025-03-11 15:27:59 +0100

    Update2

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import pyjokes 
+import pyjokes
 
-def tell_joke(): 
-    print(pyjokes.get_joke())
+def tell_joke(lang="en", category="neutral"):
+    print(pyjokes.get_joke(language=lang, category=category))
 
-if __name__ == "__main__" :
-    tell_joke()
+if __name__ == "__main__":
+    lang = input("Choisissez une langue (en, de, es, it, gl): ")
+    joke_type = input("Choisissez une catégorie de blague (neutral, chuck, all): ")
+    tell_joke(lang, joke_type)
